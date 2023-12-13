8e6c12af88824804aa7ed4708dae8ddc8193024b
Itcoderbek <128024527+Itcoderbek@users.noreply.github.com>
2023-12-13 22:06:40 +0500

    Initial commit

--- src/task_1.h
+++ src/task_1.h
@@ -1,24 +1,5 @@
-#ifndef TASK_1_H
-#define TASK_1_H
-class RedBlackTree {
-private:
-    struct Node {
-        int data;
-        char color;
-        Node* parent;
-        Node* left;
-        Node* right;
-    };
-    Node* root;
-    void insertFixup(Node* newNode);
-    void leftRotate(Node* x);
-    void rightRotate(Node* x);
-    void printTreeHelper(Node* root, int space);
-
-public:
-    RedBlackTree();
-    ~RedBlackTree();
-    void Insert(int value);
-    void PrintTree();
-};
-#endif 
+/*
+ * Author:
+ * Date:
+ * Name:
+ */
